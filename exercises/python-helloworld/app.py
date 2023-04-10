01326065ec8b68203292cf693fb8fe3998f80f9d
Khuong Nguyen <harry@LAPTOP-DA387ADD>
2023-04-11 00:29:49 +0700

    commit

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,41 @@
 from flask import Flask
+import json
+import logging
+
 app = Flask(__name__)
+
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Main request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Main request successfull')
+    return response
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
     return "Hello World!"
 
+@app.route("/xyz")
+def xyz():
+    app.logger.info('Main request successfull')
+    return "Welcome to Thiendia.com formerly known as LauXanh.US"
+
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
+
